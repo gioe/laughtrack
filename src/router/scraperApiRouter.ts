b1d14f87bcd675b1cdaeab7b9f380273d844b67d
Matt Gioe <mattgioe@Matts-MBP.lan>
2024-06-02 16:49:59 -0400

    removed currently unused route

--- src/router/scraperApiRouter.ts
+++ src/router/scraperApiRouter.ts
@@ -1,11 +1,9 @@
 import express from "express";
-import { runAllScrapers, scrapeClub } from "../controllers/scraperController.js";
+import { runAllScrapers } from "../controllers/scraperController.js";
 
 export const scraperApiRouter = express.Router();
 
 // POST items
 
 scraperApiRouter.post("/all", runAllScrapers)
-scraperApiRouter.post("/", scrapeClub)
-
 
