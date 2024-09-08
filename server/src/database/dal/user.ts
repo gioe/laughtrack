6ac98b180086e3270d4eede525f1760bf63583be
Matt Gioe <mattgioe@Matts-MBP.lan>
2024-09-07 23:16:06 -0400

    maybe fix

--- server/src/database/dal/user.ts
+++ server/src/database/dal/user.ts
@@ -9,10 +9,9 @@ export const getAdminList = async (): Promise<string[]> => {
     return downloadFile(process.env.STORAGE_BUCKET as string, 
         process.env.USERS_FILE_NAME as string)        
         .then((json: any) => {
-            return []
-            // return json[JSON_KEYS.admins].map((object: any) => {
-            //     return object[JSON_KEYS.email]
-            // })
+            return json[JSON_KEYS.admins].map((object: any) => {
+                return object[JSON_KEYS.email]
+            })
         })
 }
 
--- server/src/util/storageUtil.ts
+++ server/src/util/storageUtil.ts
@@ -4,32 +4,29 @@ import { Storage } from '@google-cloud/storage';
 import fs from 'fs/promises';
 import path from 'path';
 
+const storage = new Storage();
+
 export const downloadFile = async (bucketName: any, fileName: any) => {
     const __dirname = path.resolve();
     const cwd = path.join(__dirname, '..');
     const destination = path.join(cwd, fileName);
 
-    const storage = new Storage();
     const options = {
         destination,
     };
     
+
     await storage.bucket(bucketName).file(fileName).download(options);
 
-    // return readFile(destFileName)
+    return readFile(destination)
 }
 
 export const readFile = async (sourceFile: string): Promise<string> => {
     return fs.readFile(sourceFile, { encoding: 'utf8' })
-    .then((data: string) => JSON.parse(data))
-    .catch((err) => {
-        console.error('Error reading file:', err);
-    })
+    .then((data: string) => deleteFile(sourceFile, JSON.parse(data)))
 };
 
-export const deleteFile = async (sourceFile: string) => {
+export const deleteFile = async (sourceFile: string, data: string): Promise<string> => {
     return fs.unlink(sourceFile)
-    .catch((err) => {
-        console.error('Error deleting file:', err);
-    })
+    .then(() => data)
 };
