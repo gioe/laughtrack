4b4c680abcc450f5f8ad06cd55b961d18220ef66
Matt Gioe <mattgioe@Mac-968.lan>
2024-11-30 21:19:23 -0500

    the problem was the query

--- database/sql/page/editComedian.sql
+++ database/sql/page/editComedian.sql
@@ -2,7 +2,7 @@ SELECT
 	c.name,
 	c.id,
 	jsonb_build_object('instagram_account', instagram_account, 'instagram_followers', instagram_followers, 'tiktok_account', tiktok_account, 'tiktok_followers', tiktok_followers, 'youtube_account', youtube_account, 'youtube_followers', youtube_followers, 'website', website, 'popularity', popularity) AS social_data,
-	COALESCE(jsonb_agg(tc.id) FILTER (WHERE tc.id IS NOT NULL), '[]') AS tags
+	COALESCE(jsonb_agg(tc.tag_id) FILTER (WHERE tc.tag_id IS NOT NULL), '[]') AS tags
 FROM
 	comedians c
 	LEFT JOIN tagged_comedians tc ON c.id = tc.comedian_id
