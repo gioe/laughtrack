9f4a41de6520fccc3b61d8bc4d8780b3d37fd020
Matt Gioe <mattgioe@Mac-1032.lan>
2024-12-01 07:58:55 -0500

    setting up the flask API

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@ import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // rewrites: async () => {
+  //   return [{
+  //     source: '/api/:path*',
+  //     destination: process.env.NODE_ENV === 'development' ?
+  //       'http://127.0.0.1:5328/api/:path*' : '/api/',
+  //   }, ]
+  // },
   logging: {
     fetches: {
       fullUrl: true,
