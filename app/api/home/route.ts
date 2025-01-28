0a974a07d1a8b40de98f8b49538c6def09e7afbd
Matt Gioe <gioematt@gmail.com>
2025-01-28 10:01:46 -0500

    updated version control

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { HomePageDataResponse } from "./interface";
@@ -7,7 +6,6 @@ import { getHomePageData } from "@/lib/data/home/getHomePageData";
 export async function GET(request: Request) {
     const headersList = await headers();
     const userId = headersList.get("user_id")
-
     return getHomePageData(userId ?? undefined)
         .then((response: HomePageDataResponse) => NextResponse.json({ comedians: response.comedians, clubs: response.clubs }, { status: 200 }))
         .catch((error: Error) => NextResponse.json({ message: error.message }, { status: 500 }));
