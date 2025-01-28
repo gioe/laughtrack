5e0402064b280843fb0c6f7b54f5ea92663ee396
Matt Gioe <gioematt@gmail.com>
2025-01-27 22:05:41 -0500

    adding log just to see

--- app/api/city/route.ts
+++ app/api/city/route.ts
@@ -4,5 +4,8 @@ import { CityDTO, getCities } from "@/lib/data/cities/getCities";
 export async function GET() {
     return getCities()
         .then((cities: CityDTO[]) => NextResponse.json({ cities }, { status: 200 }))
-        .catch((error: Error) => NextResponse.json({ message: error.message }, { status: 500 }));
+        .catch((error: Error) => {
+            console.error(error.message)
+            return NextResponse.json({ message: error.message }, { status: 500 })
+        });
 }
