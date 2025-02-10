1efa746e89468ea3a5c4c5aa54dbd142fb6c66ec
Matt Gioe <gioematt@gmail.com>
2025-02-10 08:14:43 -0500

    added default params

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,18 @@
-export { auth } from "./auth";
 
 // middleware.js
-
 import { NextRequest, NextResponse } from "next/server";
 import { QueryProperty, SortParamValue } from "./objects/enum";
+import { auth } from "./auth";
+import { use } from "react";
+import { UserInterface } from "./objects/class/user/user.interface";
 
-export function middleware(request: NextRequest) {
-
+export async function middleware(request: NextRequest) {
+    const session = await auth()
+    console.log(session?.user)
     const url = new URL(request.nextUrl.pathname, request.url);
     const searchParams = new URLSearchParams(request.nextUrl.search);
 
-    setParamDefaults(searchParams, request.nextUrl.pathname)
+    setParamDefaults(searchParams, request.nextUrl.pathname, session?.user)
 
     for (const [key, value] of searchParams.entries()) {
         url.searchParams.set(key, value);
@@ -18,12 +20,18 @@ export function middleware(request: NextRequest) {
     return NextResponse.rewrite(url)
 }
 
-export function setParamDefaults(params: URLSearchParams, path: string): URLSearchParams {
+export function setParamDefaults(params: URLSearchParams, path: string, user?: UserInterface): URLSearchParams {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
 
     if (!params.has(QueryProperty.Sort)) { getSortParamDefaultFromPath(params, path) }
     if (!params.has(QueryProperty.Page)) { params.set(QueryProperty.Page, "1") }
     if (!params.has(QueryProperty.Size)) { params.set(QueryProperty.Size, "10") }
     if (!params.has(QueryProperty.Direction)) { params.set(QueryProperty.Direction, "asc") }
+    if (!params.has(QueryProperty.Zip)) { params.set(QueryProperty.Zip, user?.zipCode ?? "10003")}
+    if (!params.has(QueryProperty.FromDate)) { params.set(QueryProperty.FromDate, today.toISOString())}
+    if (!params.has(QueryProperty.ToDate)) { params.set(QueryProperty.ToDate, tomorrow.toISOString())}
 
     return params
 }
